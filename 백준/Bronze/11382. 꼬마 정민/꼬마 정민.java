7a1713fd94ea95b7e93c906f19311e1e1242a58c
Lee Joon Yeong <votmdnjem_id@naver.com>
2023-12-20 17:45:20 +0900

    [Bronze V] Title: 꼬마 정민, Time: 208 ms, Memory: 17716 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,15 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws FileNotFoundException {
+//        System.setIn(new FileInputStream("src/input.txt")); // 제출 시 주석처리
+        Scanner sc = new Scanner(System.in);
+        long a = sc.nextLong();
+        long b = sc.nextLong();
+        long c = sc.nextLong();
+
+        System.out.println(a+b+c);
+    }
+}
