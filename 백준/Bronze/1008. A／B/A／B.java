0edac39482bad8921225ecfc88b5ef77dacf267a
Lee Joon Yeong <votmdnjem_id@naver.com>
2023-12-20 15:47:58 +0900

    [Bronze V] Title: A/B, Time: 216 ms, Memory: 17824 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,13 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws FileNotFoundException {
+//        System.setIn(new FileInputStream("src/input.txt")); // 제출 시 주석처리
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println((double) a/b);
+    }
+}
