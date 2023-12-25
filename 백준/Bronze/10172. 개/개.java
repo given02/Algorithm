03f65f6107997f42fc52a44fb4a02aa558d84a3a
Lee Joon Yeong <votmdnjem_id@naver.com>
2023-12-25 18:16:09 +0900

    [Bronze V] Title: 개, Time: 116 ms, Memory: 13872 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,16 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.time.LocalDate;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws FileNotFoundException {
+//        System.setIn(new FileInputStream("src/input.txt")); // 제출 시 주석처리
+//        Scanner sc = new Scanner(System.in);
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\");
+        System.out.println("|\"^\"`    |");
+        System.out.println("||_/=\\\\__|");
+    }
+}
