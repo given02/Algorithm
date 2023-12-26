627a050ea1630326f48c3b86659844ba7ee39001
Lee Joon Yeong <votmdnjem_id@naver.com>
2023-12-26 13:57:54 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 25.51 ms, Memory: 117 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Solution {
+    public int[] solution(int[] arr) {
+        List<Integer> list = new ArrayList<>();
+        
+        int value = -1;
+        for(int i = 0; i < arr.length; i++){
+            if(value != arr[i]){
+                list.add(arr[i]);
+            }
+            value = arr[i];
+        }
+        
+        int[] answer = new int[list.size()];
+        for(int i = 0; i < list.size(); i++){
+            answer[i] = list.get(i);
+        }
+        return answer;
+    }
+}
