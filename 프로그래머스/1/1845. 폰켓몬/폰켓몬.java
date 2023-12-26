7daa489d92df627ac37db1159d98970932bda989
Lee Joon Yeong <votmdnjem_id@naver.com>
2023-12-26 13:38:24 +0900

    [level 1] Title: 폰켓몬, Time: 2.53 ms, Memory: 77.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] nums) {
+        HashSet<Integer> hs = new HashSet<>();
+
+        for(int num : nums) {
+            hs.add(num);
+        }
+
+        if(hs.size()>nums.length/2)
+            return nums.length/2;
+
+        return hs.size();
+    }
+}
