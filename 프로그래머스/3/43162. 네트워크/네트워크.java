7e4f6abff74e9c84ac16d44eb90addd18d053d2e
Lee Joon Yeong <votmdnjem_id@naver.com>
2023-12-29 19:38:49 +0900

    [level 3] Title: 네트워크, Time: 0.08 ms, Memory: 75.9 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,26 +1,26 @@
+import java.util.*;
 class Solution {
     public int solution(int n, int[][] computers) {
         int answer = 0;
-        boolean[] isChecked = new boolean[n];
+        boolean[] visited = new boolean[n];
         
         for(int i = 0; i < n; i++){
-            if(!isChecked[i]){
-                dfs(n, computers, isChecked, i);
+            if(!visited[i]){
+                dfs(i, computers, visited, n);
                 answer++;
             }
         }
         
-        
         return answer;
     }
     
-    public void dfs(int n, int[][] computers, boolean[] isChecked, int i){
-        isChecked[i] = true;
-        
+    void dfs(int i, int[][] computers, boolean[] visited, int n){
+        visited[i] = true;
         for(int j = 0; j < n; j++){
-            if(!isChecked[j] && i != j && computers[i][j] == 1){
-                dfs(n, computers, isChecked, j);
+            if(computers[i][j] == 1 && !visited[j] && j != i){
+                dfs(j, computers, visited, n);
             }
         }
     }
+    
 }
