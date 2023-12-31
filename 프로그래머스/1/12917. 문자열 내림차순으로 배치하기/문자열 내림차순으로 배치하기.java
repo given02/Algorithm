d043d727eabc1bbd8aa30be1d5723383f1254f0e
Lee Joon Yeong <votmdnjem_id@naver.com>
2023-12-31 17:19:56 +0900

    [level 1] Title: 문자열 내림차순으로 배치하기, Time: 0.40 ms, Memory: 77.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.java
@@ -0,0 +1,8 @@
+import java.util.*;
+class Solution {
+    public String solution(String s) {
+        String[] str = s.split("");
+        Arrays.sort(str, Collections.reverseOrder());
+        return String.join("", str);
+    }
+}
