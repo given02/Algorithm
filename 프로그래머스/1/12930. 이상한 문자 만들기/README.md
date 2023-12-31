70bf9f35bcbe99cd8f20231cfc000e3fb4cf2209
Lee Joon Yeong <votmdnjem_id@naver.com>
2023-12-31 21:20:43 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.07 ms, Memory: 75.1 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/README.md
+++ 프로그래머스/1/12930. 이상한 문자 만들기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 이상한 문자 만들기 - 12930 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12930#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12930) 
 
 ### 성능 요약
 
-메모리: 72.5 MB, 시간: 0.13 ms
+메모리: 75.1 MB, 시간: 0.07 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 12월 0일 21:15:59
+2023년 12월 0일 21:20:42
 
 ### 문제 설명
 
--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -1,22 +1,11 @@
 class Solution {
     public String solution(String s) {
         StringBuilder sb = new StringBuilder();
-        
         int cnt = 0;
-        for(int i = 0; i < s.length(); i++){
-            
-            if(cnt % 2 == 0){
-                sb.append(Character.toUpperCase(s.charAt(i)));
-            } else {
-                sb.append(Character.toLowerCase(s.charAt(i)));
-            }
-            
-            if(s.charAt(i) == ' '){
-                cnt = 0;
-            } else {
-                cnt++;
-            }
-            
+        
+        for(char c : s.toCharArray()){
+            sb.append(cnt % 2 == 0 ? Character.toUpperCase(c) : Character.toLowerCase(c));
+            cnt = c == ' ' ? 0 : cnt + 1;
         }
         
         return sb.toString();
