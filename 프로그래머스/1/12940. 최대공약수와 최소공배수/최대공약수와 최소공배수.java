7eaaca7662d7144aa2e5596dd75d41ce80fe5253
Lee Joon Yeong <votmdnjem_id@naver.com>
2023-12-31 19:42:24 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.11 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -6,11 +6,9 @@ class Solution {
         for(int i = 1; i <= Math.min(n, m); i++){
             if(n % i == 0 && m % i == 0){
                 answer[0] = i;
+                answer[1] = n * m / i;
             }
         }
-        
-        // 최소 공배수
-        answer[1] = n * m / answer[0];
         
         return answer;
     }
