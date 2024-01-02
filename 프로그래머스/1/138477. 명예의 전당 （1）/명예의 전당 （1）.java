d6f1a9a2141e59d8f9ab89b704aaf5ebaed42bf8
Lee Joon Yeong <votmdnjem_id@naver.com>
2024-01-02 17:05:11 +0900

    [level 1] Title: 명예의 전당 (1), Time: 0.06 ms, Memory: 71.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int k, int[] score) {
+        int[] answer = new int[score.length];
+        ArrayList<Integer> list = new ArrayList<>();
+        
+        for(int i = 0; i < score.length; i++){
+            list.add(score[i]);
+            if(i >= k){
+                list.remove(Collections.min(list));
+            }
+            answer[i] = Collections.min(list);
+        }
+        
+        return answer;
+    }
+}
