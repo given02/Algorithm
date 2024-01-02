188436294345603bad0352fe134e32d5b16ced7b
Lee Joon Yeong <votmdnjem_id@naver.com>
2024-01-02 16:02:45 +0900

    [level 1] Title: [1차] 비밀지도, Time: 1.61 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -1,12 +1,11 @@
-import java.util.*;
 class Solution {
     public String[] solution(int n, int[] arr1, int[] arr2) {
         String[] answer = new String[n];
         
         for(int i = 0; i < n; i++){
             String str = Integer.toBinaryString(arr1[i] | arr2[i]);
-            StringBuilder sb = new StringBuilder("0".repeat(n - str.length()) + str);
-            answer[i] = sb.toString().replace("1", "#").replace("0"," ");
+            str = "0".repeat(n - str.length()) + str;
+            answer[i] = str.replace("1", "#").replace("0"," ");
         }
         
         return answer;
