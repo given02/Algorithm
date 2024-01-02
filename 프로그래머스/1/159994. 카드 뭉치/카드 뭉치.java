6e2579a935a6be6a6c41151807feca3f1d747f98
Lee Joon Yeong <votmdnjem_id@naver.com>
2024-01-02 17:33:34 +0900

    [level 1] Title: 카드 뭉치, Time: 0.01 ms, Memory: 72.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Solution {
+    public String solution(String[] cards1, String[] cards2, String[] goal) {
+        String[] answer = new String[goal.length];
+        int idx1 = 0, idx2 = 0;
+        
+        for(int i = 0; i < goal.length; i++){
+            if(cards1.length > idx1 && goal[i].equals(cards1[idx1])){
+                answer[i] = cards1[idx1++];
+            } else if(cards2.length > idx2 && goal[i].equals(cards2[idx2])){
+                answer[i] = cards2[idx2++];
+            } else {
+                return "No";
+            }
+        }
+        
+        return Arrays.equals(goal, answer) ? "Yes" : "No";
+    }
+}
