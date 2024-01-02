89e9cb79d9c610a061df61fb0cbca11dfbd31ec0
Lee Joon Yeong <votmdnjem_id@naver.com>
2024-01-02 22:45:26 +0900

    [level unrated] Title: [PCCP 기출문제] 1번 / 붕대 감기, Time: 0.10 ms, Memory: 75.5 MB -BaekjoonHub

--- 프로그래머스/unrated/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.java
+++ 프로그래머스/unrated/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.java
@@ -1,5 +1,19 @@
 class Solution {
     public int solution(int[] bandage, int health, int[][] attacks) {
+        // 체력 감소
+        // ㄴ체력이 0 이하가 되면 캐릭터 사망 
+        // ㄴ사망하면 -1 반환
+        // ㄴ공격 당하면 정해진 피해량 만큼 체력 감소
+        // ㄴ공격 당하는 순간 기술이 중지되며 체력 회복 못함
+        // ㄴ공격 당하는 순간 기술을 다시 사용하며 0으로 시작
+        
+        // 체력 회복
+        // ㄴ최대 체력 이상 회복 안됨
+        // ㄴ마지막 공격을 받았을 때까지 사망하지 않고 생존하면 남은 체력 반환
+        // ㄴ공격 받지 않는 턴에는 회복
+        // ㄴt초 동안 x만큼 회복
+        // ㄴt초 연속 회복하면 y만큼 추가 회복
+        
         int answer = health;
         int currentTime = 1;
         int bandageTime = 1;
