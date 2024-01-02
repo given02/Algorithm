ccfd31911e146017797ac8bc4e84cc90dd550ca3
Lee Joon Yeong <votmdnjem_id@naver.com>
2024-01-02 12:42:00 +0900

    [level 1] Title: 시저 암호, Time: 1.47 ms, Memory: 77.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String solution(String s, int n) {
+        String answer = "";
+        
+        StringBuilder sb = new StringBuilder();
+        for(char c : s.toCharArray()){
+            if(c == ' '){
+                sb.append(" ");
+            } else if(c + n > 122 || (c + n > 90 && c <= 90)) {
+                sb.append((char)(c + n + '0' - '0' - 26));
+            } else {
+                sb.append((char)(c + n + '0' - '0'));
+            }
+        }
+        
+        return sb.toString();
+    }
+}
