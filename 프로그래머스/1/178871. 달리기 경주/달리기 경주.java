c07f5a991ce99a18d784e36560d2e32f0f19e664
Lee Joon Yeong <votmdnjem_id@naver.com>
2024-01-08 13:15:16 +0900

    [level 1] Title: 달리기 경주, Time: 0.05 ms, Memory: 72.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class Solution {
+    public String[] solution(String[] players, String[] callings) {
+        HashMap<String, Integer> map = new HashMap<>();
+        
+        for(int i = 0; i < players.length; i++){
+            map.put(players[i], i);
+        }
+        
+        for(String player : callings){
+            int curRank = map.get(player);
+            String nextPlayer = players[curRank-1];
+            
+            map.put(player, map.get(player) - 1);
+            map.put(nextPlayer, map.get(nextPlayer) + 1);
+            players[curRank - 1] = player;
+            players[curRank] = nextPlayer;
+        }
+        
+        return players;
+    }
+}
