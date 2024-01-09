9b62fbc01df3da0a7e4f4ad4f11549b3dd0ec482
Lee Joon Yeong <votmdnjem_id@naver.com>
2024-01-09 14:17:44 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 73.6 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/README.md
+++ 프로그래머스/1/42862. 체육복/README.md
@@ -1,10 +1,10 @@
 # [level 1] 체육복 - 42862 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42862#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42862) 
 
 ### 성능 요약
 
-메모리: 80.4 MB, 시간: 0.32 ms
+메모리: 73.6 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 1월 2일 14:7:56
+2024년 1월 2일 14:17:42
 
 ### 문제 설명
 
--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -1,34 +1,29 @@
 import java.util.*;
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
-        Arrays.sort(reserve);
-        
         int answer = n;
         int[] arr = new int[n];
         
-        for(int i = 0; i < lost.length; i++){
-            arr[lost[i]-1] = -1;
+        for(int i : lost){
+            arr[i-1]--;
         }
         
-        for(int i = 0; i < reserve.length; i++){
-            if(arr[reserve[i]-1] == -1){
-                arr[reserve[i]-1] = 0;
-                reserve[i] = -1;
-            }
-        }
-        
-        for(int i = 0; i < reserve.length; i++){
-            if(reserve[i] == -1) continue;
-            
-            else if(reserve[i]-2 >= 0 && arr[reserve[i]-2] == -1)
-                arr[reserve[i]-2] = 0;
-            
-            else if(reserve[i] < arr.length && arr[reserve[i]] == -1)
-                arr[reserve[i]] = 0;
+        for(int i : reserve){
+            arr[i-1]++;
         }
         
         for(int i = 0; i < arr.length; i++){
-            answer = answer + arr[i];
+            if(arr[i] == -1){
+                if(i-1 >= 0 && arr[i-1] == 1){
+                    arr[i]++;
+                    arr[i-1]--;
+                } else if(i+1 < arr.length && arr[i+1] == 1){
+                    arr[i]++;
+                    arr[i+1]--;
+                } else {
+                    answer--;
+                }
+            }
         }
         
         return answer;
