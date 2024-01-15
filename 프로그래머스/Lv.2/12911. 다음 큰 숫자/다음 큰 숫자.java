eaefb40ff7210e1be103172b3beec7e531857925
Lee Joon Yeong <votmdnjem_id@naver.com>
2024-01-15 12:14:36 +0900

    [level Lv.2] Title: 다음 큰 숫자, Time: 0.02 ms, Memory: 52 MB -BaekjoonHub

--- 프로그래머스/Lv.2/12911. 다음 큰 숫자/다음 큰 숫자.java
+++ 프로그래머스/Lv.2/12911. 다음 큰 숫자/다음 큰 숫자.java
@@ -1,8 +1,8 @@
 class Solution {
     public int solution(int n) {
         int answer = 0;
-        
         int num = n+1;
+        
         while(answer == 0){
             int a = Integer.bitCount(n);
             int b = Integer.bitCount(num);
