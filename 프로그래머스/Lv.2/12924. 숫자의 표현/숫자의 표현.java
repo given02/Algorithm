46f7649c8c103feac4ecb41db9e6ace3aee16d34
Lee Joon Yeong <votmdnjem_id@naver.com>
2024-01-15 12:00:30 +0900

    [level Lv.2] Title: 숫자의 표현, Time: 0.73 ms, Memory: 51.7 MB -BaekjoonHub

--- 프로그래머스/Lv.2/12924. 숫자의 표현/숫자의 표현.java
+++ 프로그래머스/Lv.2/12924. 숫자의 표현/숫자의 표현.java
@@ -2,13 +2,13 @@ class Solution {
     public int solution(int n) {
         int answer = 1;
         
-        int num = 0;
+        int sum = 0;
         for(int i = 1; i <= n / 2; i++){
-            num = i;
+            sum = i;
             for(int j = i+1; j <= n / 2 + 1; j++){
-                num += j;
-                if(num >= n){
-                    if(num == n) answer++;
+                sum += j;
+                if(sum >= n){
+                    if(sum == n) answer++;
                     break;
                 }
             }
