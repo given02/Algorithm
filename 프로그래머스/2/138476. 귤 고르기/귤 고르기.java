07e511f8a05acdc6c5aa1cda1b865dc750e824ae
Lee Joon Yeong <votmdnjem_id@naver.com>
2024-02-12 19:44:43 +0900

    [level 2] Title: 귤 고르기, Time: 66.57 ms, Memory: 108 MB -BaekjoonHub

---
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class Solution {
+    public int solution(int k, int[] tangerine) {
+        int answer = 0;
+        
+        HashMap<Integer, Integer> map = new HashMap<>();
+        
+        for(int i = 0; i < tangerine.length; i++){
+            map.put(tangerine[i],map.getOrDefault(tangerine[i],0)+1);
+        }
+        
+        ArrayList<Integer> keySet = new ArrayList<>(map.keySet());
+        keySet.sort((o1, o2) -> map.get(o2).compareTo(map.get(o1)));
+
+        int sum = 0;
+        for(Integer key : keySet){
+            sum += map.get(key);
+            answer++;
+            if(sum >= k) break;
+        }
+        
+        return answer;
+    }
+}
