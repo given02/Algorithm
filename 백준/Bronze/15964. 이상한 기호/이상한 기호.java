eaedf233bf12f01ddeae1324e85283133faf89c2
Lee Joon Yeong <votmdnjem_id@naver.com>
2025-01-07 17:21:53 +0900

    [Bronze V] Title: 이상한 기호, Time: 100 ms, Memory: 14120 KB, Score: 30 point -BaekjoonHub

---
+++ 백준/Bronze/15964. 이상한 기호/이상한 기호.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        solution();
+    }
+
+    public static void solution() throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+
+        bw.write(Integer.toString((A+B)*(A-B)));
+        bw.close();
+    }
+
+}
+
