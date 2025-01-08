9fe38e4a7560f788e8f44209e86d9278350910f4
Lee Joon Yeong <votmdnjem_id@naver.com>
2025-01-08 15:56:35 +0900

    [Bronze V] Title: 구구단, Time: 100 ms, Memory: 14164 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        solution();
+    }
+
+    public static void solution() throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+
+        for(int i = 1; i <= 9; i++){
+            bw.write(N + " * " + i + " = " + (N*i) + "\n");
+        }
+
+        bw.close();
+    }
+
+}
+
