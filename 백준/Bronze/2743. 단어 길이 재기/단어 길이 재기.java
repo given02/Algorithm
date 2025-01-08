9480dd27af879594fbe43a80a99540ecac5c7281
Lee Joon Yeong <votmdnjem_id@naver.com>
2025-01-08 16:16:45 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 104 ms, Memory: 14216 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        solution();
+    }
+
+    public static void solution() throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+
+        String str = st.nextToken();
+        bw.write(str.length() + "");
+
+        bw.close();
+
+    }
+
+}
+
