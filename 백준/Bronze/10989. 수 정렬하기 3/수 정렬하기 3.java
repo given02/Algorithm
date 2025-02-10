af59149229d40d545225b744ed5520e36e796a4b
Lee Joon Yeong <votmdnjem_id@naver.com>
2025-02-10 14:02:49 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 2548 ms, Memory: 346360 KB -BaekjoonHub

---
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        solution();
+    }
+
+    public static void solution() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[N];
+        for(int i = 0; i < N; i++){
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(arr);
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        for(int i = 0; i < N; i++){
+            bw.write(arr[i] + "\n");
+        }
+
+        bw.close();
+        br.close();
+    }
+
+}
