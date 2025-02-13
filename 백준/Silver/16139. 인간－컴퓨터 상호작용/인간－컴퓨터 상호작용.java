05357da7390808bcf20ff3b0e7a62eccafb6cfe9
Lee Joon Yeong <votmdnjem_id@naver.com>
2025-02-13 14:17:47 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 812 ms, Memory: 127756 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.java
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.java
@@ -9,36 +9,35 @@ public class Main {
 
     public static void solution() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        String[] str = br.readLine().split("");
+        String str = br.readLine();
         int N = Integer.parseInt(br.readLine());
+        int[][] S = new int[str.length()+1][26];
+        for(int i = 1; i <= str.length(); i++){
+            int searchChar = str.charAt(i-1)-'a';
 
-        StringTokenizer st;
-        String[][] arr = new String[N][3];
-        for(int i = 0; i < N; i++){
-            st = new StringTokenizer(br.readLine());
-            arr[i][0] = st.nextToken();
-            arr[i][1] = st.nextToken();
-            arr[i][2] = st.nextToken();
-        }
-
-        int[] S = new int[N];
-        for(int i = 0; i < N; i++){
-            int start = Integer.parseInt(arr[i][1]);
-            int end = Integer.parseInt(arr[i][2]);
-            for (int j = start; j <= end; j++) {
-                if (arr[i][0].equals(str[j])) {
-                    S[i] += 1;
+            for(int j = 0; j < 26; j++){
+                if(searchChar == j){
+                    S[i][j] = S[i-1][j] + 1;
+                } else {
+                    S[i][j] = S[i-1][j];
                 }
             }
         }
-        
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        for(int i : S){
-            bw.write(i + "\n");
+
+        StringTokenizer st;
+        for(int i = 0; i < N; i++){
+            st = new StringTokenizer(br.readLine());
+
+            int a = st.nextToken().charAt(0)-'a';
+            int start = Integer.parseInt(st.nextToken())+1;
+            int end = Integer.parseInt(st.nextToken())+1;
+
+            bw.write(S[end][a] - S[start-1][a] + "\n");
         }
 
-        bw.close();
+        bw.flush();
         br.close();
     }
 
