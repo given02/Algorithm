51c2d4462c2626bfd8c9707789094a21ff4e5e26
Lee Joon Yeong <votmdnjem_id@naver.com>
2025-04-02 14:28:08 +0900

    [level 3] Title: 입국심사, Time: 133.65 ms, Memory: 93.8 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -1,25 +1,27 @@
 import java.util.*;
+
 class Solution {
     public long solution(int n, int[] times) {
         Arrays.sort(times);
         
         long answer = 0;
         long left = 0;
-        long right = (long) n * times[times.length - 1]; 
+        long right = (long) times[times.length-1] * n;
         while(left <= right){
-            long mid = (left + right) / 2;
             long sum = 0;
+            long mid = (left + right) / 2;
             for(int i = 0; i < times.length; i++){
                 sum += mid / times[i];
             }
+            
             if(sum < n){
                 left = mid + 1;
-            } else {
+            }
+            else {
                 right = mid - 1;
                 answer = mid;
             }
         }
-        
         
         return answer;
     }
