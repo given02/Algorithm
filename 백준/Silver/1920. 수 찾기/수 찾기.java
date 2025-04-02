0083292bbe84d4940a38ed0a1f96a022202cbe0e
Lee Joon Yeong <votmdnjem_id@naver.com>
2025-04-02 13:51:06 +0900

    [Silver IV] Title: 수 찾기, Time: 1100 ms, Memory: 54568 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,32 +1,58 @@
 import java.io.*;
 import java.util.*;
 
-public class Main {
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.io.BufferedReader;
+import java.io.IOException;
 
+public class Main {
+    
     public static void main(String[] args) throws IOException {
         solution();
     }
 
     public static void solution() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
         HashSet<Integer> set = new HashSet<>();
 
         int N = Integer.parseInt(br.readLine());
-        st = new StringTokenizer(br.readLine());
+        int[] A = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
         for(int i = 0; i < N; i++){
-            set.add(Integer.valueOf(st.nextToken()));
+            A[i] = Integer.parseInt(st.nextToken());
         }
+
+        Arrays.sort(A);
 
         int M = Integer.parseInt(br.readLine());
         st = new StringTokenizer(br.readLine());
         for(int i = 0; i < M; i++){
-            if(set.contains(Integer.parseInt(st.nextToken()))){
-                System.out.println(1);
-            } else {
-                System.out.println(0);
+            boolean find = false;
+            int target = Integer.parseInt(st.nextToken());
+            int start = 0;
+            int end = A.length - 1;
+            while(start <= end){
+                int mid_index = (start+end) / 2;
+                int mid_value = A[mid_index];
+                if(mid_value > target){
+                    end = mid_index - 1;
+                }
+                else if(mid_value < target){
+                    start = mid_index + 1;
+                }
+                else {
+                    find = true;
+                    break;
+                }
             }
+
+            if(find) System.out.println(1);
+            else System.out.println(0);
         }
+
 
     }
 
