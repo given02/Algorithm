7721b25e5b9cf806f38df2fc723f9a3ad4cc0f14
Lee Joon Yeong <votmdnjem_id@naver.com>
2025-04-07 17:24:40 +0900

    [level 4] Title: 사칙연산, Time: 28.41 ms, Memory: 56.1 MB -BaekjoonHub

--- 프로그래머스/4/1843. 사칙연산/사칙연산.java
+++ 프로그래머스/4/1843. 사칙연산/사칙연산.java
@@ -2,52 +2,59 @@ import java.util.*;
 
 class Solution {
     
-    public int solution(String arr[]) {
-        int N = arr.length/2+1;
+    int[][][] dp;
+    int[] nums;
+    int[] ops; // 0: +, 1: -
+    
+    public int solution(String[] arr) {
+        int N = arr.length / 2 + 1;
         
-        int[] nums = new int[N];
-        int[] ops = new int[N-1];
+        nums = new int[N];
+        ops = new int[N - 1];
         
-        int idx1 = 0;
-        int idx2 = 0;
-        for(String s : arr){
-            if(s.equals("+")){
-                ops[idx2++] = 0;
-            } else if(s.equals("-")){
-                ops[idx2++] = 1;
-            }else {
-                nums[idx1++] = Integer.parseInt(s);
+        int idx1 = 0, idx2 = 0;
+        for (String s : arr) {
+            if (s.equals("+")) ops[idx2++] = 0;
+            else if (s.equals("-")) ops[idx2++] = 1;
+            else nums[idx1++] = Integer.parseInt(s);
+        }
+
+        // dp 초기화. -1: 아직 계산 안 됨
+        dp = new int[N][N][2];
+        for (int[][] a : dp)
+            for (int[] b : a)
+                Arrays.fill(b, Integer.MIN_VALUE); // 최소는 MIN_VALUE로, 최대는 -MIN_VALUE로
+        
+        return DP(0, N - 1, 1); // 전체 범위의 최대값 계산
+    }
+
+    private int DP(int s, int e, int isMax) {
+        if (s == e) return nums[s]; // 숫자 하나 남은 경우
+        if (dp[s][e][isMax] != Integer.MIN_VALUE) return dp[s][e][isMax];
+        
+        int res = isMax == 1 ? Integer.MIN_VALUE : Integer.MAX_VALUE;
+
+        for (int k = s; k < e; k++) {
+            int op = ops[k];
+            int leftMax = DP(s, k, 1);
+            int leftMin = DP(s, k, 0);
+            int rightMax = DP(k + 1, e, 1);
+            int rightMin = DP(k + 1, e, 0);
+            
+            if (op == 0) { // +
+                if (isMax == 1)
+                    res = Math.max(res, leftMax + rightMax);
+                else
+                    res = Math.min(res, leftMin + rightMin);
+            } else { // -
+                if (isMax == 1)
+                    res = Math.max(res, leftMax - rightMin);
+                else
+                    res = Math.min(res, leftMin - rightMax);
             }
         }
-        
-        int[][][] dp = new int[N][N][2];
-        // 자기자신만 괄호친 경우
-        for(int i = 0; i < N; i++){
-            dp[i][i][0] = dp[i][i][1] = nums[i];
-        }
-        
-        for(int i = 1; i < N; i++){ // 0,0은 초기화 됐으니까 0,1부터 시작하기 위해 i=1
-            for(int s = 0; s < N; s++){
-                if(i + s >= N) break;
-                int e = i + s;
-                int max = Integer.MIN_VALUE;
-                int min = Integer.MAX_VALUE;
-                for(int j = s; j < e; j++){
-                    if(ops[j] == 0){
-                        max = Math.max(max, dp[s][j][1] + dp[j+1][e][1]);
-                        min = Math.min(min, dp[s][j][0] + dp[j+1][e][0]);
-                    }
-                    else {
-                        max = Math.max(max, dp[s][j][1] - dp[j+1][e][0]);
-                        min = Math.min(min, dp[s][j][0] - dp[j+1][e][1]);
-                    }
-                }
-                dp[s][e][0] = min;
-                dp[s][e][1] = max;
-            }
-        }
-        
-        return dp[0][N-1][1];
+
+        return dp[s][e][isMax] = res;
     }
     
 }
